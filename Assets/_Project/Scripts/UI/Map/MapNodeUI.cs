9b8a321e9665e349843e5e8f7df0a22a75edc39c
Brian <brianlautaro@hotmail.com>
2026-02-09 23:55:45 -0300

    Style: Map 2

--- Assets/_Project/Scripts/UI/Map/MapNodeUI.cs
+++ Assets/_Project/Scripts/UI/Map/MapNodeUI.cs
@@ -6,6 +6,11 @@ public class MapNodeUI : MonoBehaviour
 {
     [SerializeField] private Button button;
     [SerializeField] private TextMeshProUGUI label;
+    [SerializeField] private Image nodeIconImage;
+    [SerializeField] private Sprite combatSprite;
+    [SerializeField] private Sprite eventSprite;
+    [SerializeField] private Sprite shopSprite;
+    [SerializeField] private Sprite bossSprite;
 
     private MapNodeData nodeData;
 
@@ -13,6 +18,22 @@ public class MapNodeUI : MonoBehaviour
     {
         nodeData = data;
         label.text = data.title;
+        Sprite selectedSprite = data.nodeType switch
+        {
+            NodeType.Combat => combatSprite,
+            NodeType.Event => eventSprite,
+            NodeType.Shop => shopSprite,
+            NodeType.Boss => bossSprite,
+            _ => null
+        };
+
+        if (nodeIconImage != null)
+        {
+            nodeIconImage.sprite = selectedSprite;
+            nodeIconImage.preserveAspect = true;
+        }
+
+        button.onClick.RemoveAllListeners();
         button.onClick.AddListener(() => callback?.Invoke(nodeData));
     }
 }
